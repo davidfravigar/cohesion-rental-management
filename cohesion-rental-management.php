7825a504130b786abb8d6f96c4fa66e946acf134
David Fravigar <david@tjs.com>
2016-06-03 08:55:22 +0100

    init commit and register the plugin with wordpress

---
+++ cohesion-rental-management.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * -------------------------------------------------------------------------------------------------
+ * Wordpress
+ * -------------------------------------------------------------------------------------------------
+ * Plugin Name: Cohesion Rental Management
+ * Plugin URI: http://davidfravigar.com/
+ * Description: A plugin for administering rental and property management
+ * Version: 0.0.1
+ * Author: David Fravigar
+ * Author URI: http://davidfravigar.com/
+ * License: GNU GENERAL PUBLIC LICENSE V3
+ * -------------------------------------------------------------------------------------------------
+ * Developer
+ * -------------------------------------------------------------------------------------------------
+ * Hey hi there developer! this is a plugin that creates several custom post types, metaboxes and
+ * taxonomies that are used with a frontend editor.
+ *
+ * This plugin has been constructed so that it is modular but you will have to get your hands dirty
+ * in order to extend it past it's original state.
+ *
+ * Please keep this file clean as most of the heavy lifting takes place within the framework itself
+ * this file is where we configure everything and send it to the framework to create.
+ * @author 		David Fravigar <david.fravigar@me.com>
+ * @version  	0.0.1
+ * -------------------------------------------------------------------------------------------------
+ */
